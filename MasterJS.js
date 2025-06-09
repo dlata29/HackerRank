de89fab9479f61d106176e5cb891f65479808796
dlata29 <divya_9629@yahoo.com>
2025-06-09 06:23:29 +0530

    a

--- MasterJS.js
+++ MasterJS.js
@@ -1,22 +1,3 @@
-// const tabs = document.querySelectorAll(".subTab");
-// const contents = document.querySelectorAll(".contentBox");
-
-// tabs.forEach((tab) => {
-//   tab.addEventListener("click", () => {
-//     // Remove all active styles and hide all contents
-//     tabs.forEach((t) => t.classList.remove("activeHeader"));
-//     contents.forEach((c) => (c.style.display = "none"));
-
-//     // Add active styles to clicked tab and show its content
-//     tab.classList.add("activeHeader");
-//     document.getElementById(tab.dataset.content).style.display = "block";
-//   });
-// });
-
-document.getElementById("openModal").onclick = () => {
-  document.getElementById("modalBackdrop").classList.add("show");
-};
-
-document.querySelector(".close-btn").onclick = () => {
-  document.getElementById("modalBackdrop").classList.remove("show");
-};
+const timer = document.querySelector(".watch");
+const btn = document.querySelector(".start");
+const text = document.querySelector(".textTime");
