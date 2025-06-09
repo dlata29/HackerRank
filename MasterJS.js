249800a95e4d4000d498030e142d037ac05d0fe3
dlata29 <divya_9629@yahoo.com>
2025-06-09 06:13:04 +0530

    a

--- MasterJS.js
+++ MasterJS.js
@@ -1,14 +1,22 @@
-const tabs = document.querySelectorAll(".subTab");
-const contents = document.querySelectorAll(".contentBox");
+// const tabs = document.querySelectorAll(".subTab");
+// const contents = document.querySelectorAll(".contentBox");
 
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    // Remove all active styles and hide all contents
-    tabs.forEach((t) => t.classList.remove("activeHeader"));
-    contents.forEach((c) => (c.style.display = "none"));
+// tabs.forEach((tab) => {
+//   tab.addEventListener("click", () => {
+//     // Remove all active styles and hide all contents
+//     tabs.forEach((t) => t.classList.remove("activeHeader"));
+//     contents.forEach((c) => (c.style.display = "none"));
 
-    // Add active styles to clicked tab and show its content
-    tab.classList.add("activeHeader");
-    document.getElementById(tab.dataset.content).style.display = "block";
-  });
-});
+//     // Add active styles to clicked tab and show its content
+//     tab.classList.add("activeHeader");
+//     document.getElementById(tab.dataset.content).style.display = "block";
+//   });
+// });
+
+document.getElementById("openModal").onclick = () => {
+  document.getElementById("modalBackdrop").classList.add("show");
+};
+
+document.querySelector(".close-btn").onclick = () => {
+  document.getElementById("modalBackdrop").classList.remove("show");
+};
