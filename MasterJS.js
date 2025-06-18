c00e98a60cb4adb056ed74cb0bc4999fe82cf8d6
dlata29 <divya_9629@yahoo.com>
2025-06-18 20:25:43 +0530

    1

--- MasterJS.js
+++ MasterJS.js
@@ -12,19 +12,13 @@ async function loadUsers() {
     const data = await response.json();
 
     console.log(data);
-    for (i = 0; i < data.length; i++) {
-      const user = data[i];
 
-      const cardWrapper = document.createElement("div");
-      cardWrapper.className = "col-md-6 mb-4";
-
+    data.forEach((item) => {
       const card = document.createElement("div");
-      card.className = "card p-3 shadow-sm text-center";
-      card.textContent = user.name;
-
-      cardWrapper.appendChild(card);
-      container.appendChild(cardWrapper);
-    }
+      card.className = "card";
+      card.textContent = item.name;
+      container.appendChild(card);
+    });
   } catch (err) {
     container.innerHTML = "<p class='text-danger'>Error loading users.</p>";
     console.error(err);
