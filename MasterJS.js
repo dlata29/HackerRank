a4e99f592c4a074b5b7cbbe235b66e17d9504d39
dlata29 <divya_9629@yahoo.com>
2025-06-18 20:03:25 +0530

    1

--- MasterJS.js
+++ MasterJS.js
@@ -10,21 +10,30 @@ async function loadVals() {
     }
 
     const data = await response.json();
-
     const container = document.getElementById("ans");
-
-    for (i = 0; i < data.length; i++) {
-      if (i % 3 === 0) {
-        var row = document.createElement("div");
-        row.className = "row";
-        container.appendChild(row);
-      }
-
+    data.forEach((item) => {
+      // show 9 items total = 3 rows
       const card = document.createElement("div");
       card.className = "card";
-      card.textContent = data[i].name;
-      row.appendChild(card);
-    }
+      card.textContent = item.name;
+      container.appendChild(card);
+    });
+    // const data = await response.json();
+
+    // const container = document.getElementById("ans");
+
+    // for (i = 0; i < data.length; i++) {
+    //   if (i % 3 === 0) {
+    //     var row = document.createElement("div");
+    //     row.className = "row";
+    //     container.appendChild(row);
+    //   }
+
+    //   const card = document.createElement("div");
+    //   card.className = "card";
+    //   card.textContent = data[i].name;
+    //   row.appendChild(card);
+    // }
   } catch (error) {
     console.log("error in main", error);
   }
