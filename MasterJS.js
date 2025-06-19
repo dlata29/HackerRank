2f527ee455081e242e82d8f514cc7f2d6fdc8079
dlata29 <divya_9629@yahoo.com>
2025-06-19 07:17:55 +0530

    1

--- MasterJS.js
+++ MasterJS.js
@@ -1,5 +1,9 @@
 const container = document.getElementById("userContainer");
+const pagination = document.getElementById("pagination");
 const url = "https://jsonplaceholder.typicode.com/users";
+
+const usersPerPage = 4;
+let users = [];
 
 async function loadUsers() {
   try {
@@ -9,19 +13,44 @@ async function loadUsers() {
       throw new Error("threw error");
     }
 
-    const data = await response.json();
-
-    console.log(data);
-
-    data.forEach((item) => {
-      const card = document.createElement("div");
-      card.className = "card";
-      card.textContent = item.name;
-      container.appendChild(card);
-    });
+    users = await response.json();
+    showPage(1);
+    createPaginationButtons();
   } catch (err) {
     container.innerHTML = "<p class='text-danger'>Error loading users.</p>";
     console.error(err);
+  }
+}
+
+function showPage(num) {
+  container.innerHTML = "";
+  const start = (num - 1) * usersPerPage;
+  const end = usersPerPage + start;
+  const paginatedUsers = users.slice(start, end);
+
+  users.forEach((user) => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.textContent = user.name;
+    container.appendChild(card);
+  });
+
+  const allButtons = document.querySelectorAll("#pagination button");
+  allButtons.forEach((btn) => btn.classList.remove("active"));
+  if (allButtons[page - 1]) {
+    allButtons[page - 1].classList.add("active");
+  }
+}
+
+function createPaginationButtons() {
+  const totalPages = Math.ceil(users.length / usersPerPage);
+  pagination.innerHTML = "";
+
+  for (let i = 1; i < totalPages; i++) {
+    const btn = document.createElement("button");
+    btn.textContent = i;
+    btn.addEventListener("click", () => showPage(i));
+    pagination.appendChild(btn);
   }
 }
 
