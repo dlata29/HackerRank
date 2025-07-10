e66def1783418153df443f8fad0d4770ff04bed2
dlata29 <divya_9629@yahoo.com>
2025-07-10 06:34:51 +0530

    1

--- MasterJS.js
+++ MasterJS.js
@@ -1,7 +1,7 @@
 const p1 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("p1 resole");
-  }, 3000);
+    reject("p1 resole");
+  }, 1000);
 });
 const p2 = new Promise((resolve, reject) => {
   setTimeout(() => {
@@ -10,12 +10,12 @@ const p2 = new Promise((resolve, reject) => {
 });
 const p3 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("p3 resole");
-  }, 2000);
+    reject("p3 resole");
+  }, 1000);
 });
 
 Promise.any([p1, p2, p3])
   .then((res) => {
     console.log(res);
   })
-  .catch((err) => console.error(err));
+  .catch((err) => console.error(err.errors));
