14a4d5675bc0056a989d402af95c3c9ad5e8b696
dlata29 <divya_9629@yahoo.com>
2025-08-15 19:06:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
-// function Bank(initial) {
-//   this.initial = initial;
-// }
-
-// Bank.prototype.add = function (amount) {
-//   return this.initial + amount;
-// };
-
-class Bank {
-  constructor() {
-    this.initial = 0;
-  }
-
-  add(amount) {
-    return (this.initial = this.initial + amount);
-  }
-  get bal() {
-    return this.initial;
+function askQ(question, yes, no) {
+  if (confirm(question)) {
+    yes();
+  } else {
+    no();
   }
 }
 
-const p1 = new Bank();
-console.log(p1.add(200));
-const p2 = new Bank();
-console.log(p2.add(100));
-console.log(p2.bal);
-console.log(p1.bal);
+function showYes() {
+  console.log("yay");
+}
+
+function showNo() {
+  console.log("nooo");
+}
+
+askQ("do you agree", showYes, showNo);
