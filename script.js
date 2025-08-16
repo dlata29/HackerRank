f4d18e2f09f7ea6beccabb847c39074e2f7a6724
dlata29 <divya_9629@yahoo.com>
2025-08-16 06:44:03 +0530

    1

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-"use strict";
-let age = prompt("What is your age?", 18);
-
-// conditionally declare a function
-if (age < 18) {
-  function aa() {
-    console.log("Hello!");
-  }
-} else {
-  function aa() {
-    console.log("Greetings!");
-  }
-}
-
-// ...use it later
-console.log(aa()); // Error: welcome is not defined
+//
