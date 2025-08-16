be6417998af747f4488ea2e93a1303cf37ce379a
dlata29 <divya_9629@yahoo.com>
2025-08-16 07:51:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//
+// Object spread
+const defaults = {
+  theme: "dark",
+  language: "en",
+  sayhi: function () {
+    let x = 5;
+    return x;
+  },
+};
+
+const obj1 = structuredClone(defaults);
+console.log(obj1);
