2b7d87ed82b27bd40fccbdf2766ae81a73e25c4f
dlata29 <divya_9629@yahoo.com>
2025-08-21 07:08:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,36 @@ const taskInput = document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
 const filterBtns = document.querySelectorAll(".filters button");
+
+function render() {
+  taskList.innerHTMLhtml = "";
+
+  let filteredTasks = tasks.filter((task) => {
+    if (currentFilter === "active") return !task.completed;
+    if (currentFilter === "completed") return task.completed;
+    return true; // all
+  });
+
+  filteredTasks.forEach((task) => {
+    const li = document.createElement("li");
+    li.className = "task";
+    li.innerHTML = `
+    <span class="${task.completed ? "completed" : " "}">
+    
+    `;
+    taskList.textContent += task.ip;
+  });
+}
+function addTask(ip) {
+  tasks.push({ id: Date.now(), ip, completed: false });
+  taskInput.value = "";
+  render();
+}
+
+addBtn.addEventListener("click", () => {
+  const ip = taskInput.value.trim();
+
+  if (!ip) return;
+
+  addTask(ip);
+});
