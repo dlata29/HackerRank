0b2fb6bc3f7599409ae1dda47951ee58d807c5c0
dlata29 <divya_9629@yahoo.com>
2025-08-21 06:21:48 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let tasks = [];
+let currentFilter = "all";
+
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+const filterBtns = document.querySelectorAll(".filters button");
