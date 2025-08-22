e1c34656763200cde06abe5a8c3089564c0159d3
dlata29 <divya_9629@yahoo.com>
2025-08-22 08:17:20 +0530

    1

---
+++ script.js
@@ -0,0 +1,19 @@
+let countries = []; // will hold API data
+
+const input = document.getElementById("searchBox");
+const suggestionList = document.getElementById("suggestions");
+
+async function loadCountries() {
+  const API_URL = "https://countriesnow.space/api/v0.1/countries";
+  try {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    // console.log(data.data);
+
+    countries = data.data.map((c) => (c = c.country));
+    console.log("Loaded countries:", countries.length);
+  } catch (err) {
+    console.error("Failed to load countries:", err.message);
+  }
+}
+loadCountries().then((res) => printFirst(12));
