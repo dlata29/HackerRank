d9a824c34cc43c9853770b2c655c8d81f8549973
dlata29 <divya_9629@yahoo.com>
2025-09-17 07:28:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const user = { profile: { name: "Divya" } };
 console.log(user.profile?.name); // Divya
-console.log(user.account.balance); // undefined (no error!)
+console.log(user.account?.balance); // undefined (no error!)
