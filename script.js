e23037679b57f10c06a62e6c32840e1ae5c29dda
dlata29 <divya_9629@yahoo.com>
2025-09-17 07:19:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-class Animal {
-  constructor(type) {
-    this.type = type;
-  }
-
-  walk() {
-    console.log(this.type, "is walking");
-  }
-}
-
-class Dog extends Animal {
-  constructor(nm, name) {
-    super(nm);
-    this.name = name;
-  }
-}
-
-const c1 = new Dog("divya", "saurav");
-c1.walk();
+const user = { profile: { name: "Divya" } };
+console.log(user.profile?.name); // Divya
+console.log(user.account?.balance); // undefined (no error!)
