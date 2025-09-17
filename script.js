14c197c87303c10929c59a3908fe14385bb65b80
dlata29 <divya_9629@yahoo.com>
2025-09-17 06:02:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const user = {
-  name: "Divya",
-  age: 25,
-  greet() {
-    console.log("Hello " + this.name); // ✅ `this` refers to `user`
-  },
-  updateAge(newAge) {
-    this.age = newAge; // ✅ modifies `user.age`
-  },
-};
-user.greet();
-
+function multiply(a, b) {
+  return a * b;
+}
